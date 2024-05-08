6f20bb34ceb5abe1d6693258f691cb014271060b
achint2093 <=achintwadhwa418@gmail.com>
2024-05-08 10:28:36 +0530

    initial commit

---
+++ app/src/main/java/com/techuntried/notes/model/NoteEntity.kt
@@ -0,0 +1,13 @@
+package com.techuntried.notes.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "notes")
+data class NoteEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id:Long,
+    @ColumnInfo("")
+    val folderName:String
+)
---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+buildscript {
+    repositories {
+        google()
+    }
+    dependencies {
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.5")
+
+    }
+}
+
+
+plugins {
+    id("com.android.application") version "8.1.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
+    id("com.android.library") version "8.1.2" apply false
+    id("com.google.dagger.hilt.android") version "2.48" apply false
+}
+
